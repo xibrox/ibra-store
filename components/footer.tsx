640ef78913a91098a8a93f756561ff4f4512ffdb
xibrox <batonchik2004@gmail.com>
2023-07-07 21:20:16 +0200

    fix name

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2023 Ibro.cz
+                    &copy; 2023 IBRA.cz
                 </p>
             </div>
         </footer>
