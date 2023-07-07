4e828a5d38f82b6c64d6d5ea5d8e0bfc1c1e04cb
xibrox <batonchik2004@gmail.com>
2023-07-07 21:12:37 +0200

    final

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { Urbanist } from 'next/font/google'
+
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+
+import './globals.css'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'E-Shop',
+  description: 'E-Shop',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="cs">
+      <body className={font.className}>
+        <ModalProvider />
+        <ToastProvider />
+        {/* @ts-ignore */}
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ components/ui/currency.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export const formatter = new Intl.NumberFormat("cs", {
+    style: 'currency',
+    currency: 'CZK'
+});  
+
+interface CurrencyProps {
+    value?: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({
+    value
+}) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    
+    if (!isMounted) {
+        return null;
+    }
+
+    return ( 
+        <div className="font-semibold">
+            {formatter.format(Number(value))}
+        </div>
+    );
+}
+ 
+export default Currency;
